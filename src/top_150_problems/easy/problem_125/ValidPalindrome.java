731f36936fcab2fda5d04937e8b5894a3a248b7e
Hasibul Hassan <shahi_aust@hotmail.com>
2025-09-03 23:39:27 +0800

    Update ValidPalindrome.java

--- src/top_150_problems/easy/problem_125/ValidPalindrome.java
+++ src/top_150_problems/easy/problem_125/ValidPalindrome.java
@@ -7,9 +7,10 @@ public class ValidPalindrome {
     public ValidPalindrome() {
         System.out.println("****************** 125. Valid Palindrome *******************");
         String s = "0P";
-        System.out.println(this.isPalindrome(s));
+        System.out.println(this.isPalindromeWithoutTwoPointer(s));
+        System.out.println(this.isPalindromeWithTwoPointer(s));
     }
-    public boolean isPalindrome(String s) {
+    public boolean isPalindromeWithoutTwoPointer(String s) {
         s = s.toLowerCase();
         char[] ch = s.toCharArray();
         char[] str = new char[s.length()];
@@ -24,5 +25,29 @@ public class ValidPalindrome {
         String revStr = new StringBuilder(cleanString).reverse().toString();
         System.out.println(cleanString+" -> "+revStr);
         return cleanString.equals(revStr);
+    }
+
+    public boolean isPalindromeWithTwoPointer(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // Skip non-alphabetic characters
+            while (left < right && !(s.charAt(left) >= 'a' && s.charAt(left) <= 'z' || s.charAt(left) >= '0' && s.charAt(left) <='9')) {
+                left++;
+            }
+            while (left < right && !(s.charAt(right) >= 'a' && s.charAt(right) <= 'z' || s.charAt(left) >= '0' && s.charAt(left) <='9')) {
+                right--;
+            }
+
+            // Compare characters
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
